ac41b514d41d730b71df3a8444a94d085f27726f
rotcivegaf <victorfage@gmail.com>
2024-07-12 15:19:00 -0300

    remove console

--- contracts/src/Eggs.sol
+++ contracts/src/Eggs.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
 import { ERC1155 } from "solmate/tokens/ERC1155.sol";
 import { Owned } from "solmate/auth/Owned.sol";
 
