a2039bffb76be7a4cb08032ff8690355e3df066a
rotcivegaf <victorfage@gmail.com>
2024-07-12 14:55:38 -0300

    Up contracts

---
+++ contracts/src/Fleekeggs.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import { ERC1155 } from "solmate/tokens/ERC1155.sol";
+import { Owned } from "solmate/auth/Owned.sol";
+
+interface IUriContract {
+    function uri(uint256 id) external view returns (string memory);
+}
+
+contract Fleekeggs is ERC1155, Owned {
+    event SetUriContract(IUriContract uriContract);
+    event SetMinZeros(uint256 minZeros);
+
+    bytes32 constant private F = 0xF000000000000000000000000000000000000000000000000000000000000000;
+    string public name = "Fleekeggs";
+    string public symbol = "FLEEKG";
+
+    uint256 public minZeros;
+    IUriContract public uriContract;
+
+    mapping(address => bytes32) public usersHash;
+
+    constructor (uint256 _minZeros) Owned(msg.sender) {
+        require(_minZeros < 64, "Too much zeros");
+        minZeros = _minZeros;
+        emit SetMinZeros(_minZeros);
+    }
+
+    function setMinZeros(uint256 _minZeros) external onlyOwner {
+        require(_minZeros < 64, "Too much zeros");
+        minZeros = _minZeros;
+        emit SetMinZeros(_minZeros);
+    }
+
+    function setUriContract(IUriContract _uriContract) external onlyOwner {
+        uriContract = _uriContract;
+        emit SetUriContract(_uriContract);
+    }
+
+    function uri(uint256 id) public override view returns (string memory) {
+        return uriContract.uri(id);
+    }
+
+    function mintBatch(
+        address to,
+        bytes32[] calldata nonces
+    ) external {
+        uint256 noncesLength = nonces.length;
+        uint256[] memory amounts = new uint256[](64);
+
+        for (uint256 i; i < noncesLength; ++i) {
+            uint256 zeros = _countZeros(
+                keccak256(
+                    abi.encodePacked(block.chainid, msg.sender, usersHash[to], to, i, nonces[i])
+                )
+            );
+            if (zeros >= minZeros) {
+                ++amounts[zeros];
+            }
+        }
+
+        usersHash[to] = blockhash(block.number - 1);
+
+        uint256 amountsLength = amounts.length;
+        for (uint256 i = minZeros; i < amountsLength; ++i) {
+            _mint(
+                to,
+                i - minZeros,
+                amounts[i],
+                ""
+            );
+        }
+    }
+
+    function _countZeros(bytes32 hash) private pure returns(uint256 zeros) {
+        unchecked {
+            for (uint256 i; i < 64; ++i) {
+                uint256 shiftBits = i * 4;
+
+                if (hash << shiftBits & F == 0) {
+                    ++zeros;
+                } else {
+                    return zeros;
+                }
+            }
+        }
+    }
+
+}
---
+++ contracts/test/Fleekeggs.t.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import {Fleekeggs} from "../src/Fleekeggs.sol";
+
+contract FleekeggsTest is Test {
+    Fleekeggs fleekeggs;
+
+    address owner = address(0xFF);
+    address user = 0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D;
+
+    function setUp() public {
+        vm.prank(owner);
+        fleekeggs = new Fleekeggs(2);
+    }
+
+    function test_constructor() public view {
+        assertEq(fleekeggs.owner(), owner);
+    }
+
+    function test_mintBatch_four_ids_1() public {
+        bytes32[] memory nonces = new bytes32[](8);
+
+        nonces[0] = 0x000000000000000000000000000000000000000000000000000053e45bd229ac; // 3 zeros
+        nonces[1] = 0x00000000000000000000000000000000000000000000000000000bc6e541bf7b; // 3 zeros
+        nonces[2] = 0x000000000000000000000000000000000000000000000000000003fc5d630f92; // 3 zeros
+        nonces[3] = 0x00000000000000000000000000000000000000000000000000004688655ff8cf; // 3 zeros
+        nonces[4] = 0x0000000000000000000000000000000000000000000000000000000000000001; // non-zeros
+        nonces[5] = bytes32(0); // non-zeros
+        nonces[6] = 0x0000000000000000000000000000000000000000000000000000000000000003; // non-zeros
+        nonces[7] = 0x0000000000000000000000000000000000000000000000000000000000000002; // non-zeros
+
+        vm.prank(user);
+        fleekeggs.mintBatch(
+            user,
+            nonces
+        );
+
+        assertEq(fleekeggs.balanceOf(user, 0), 0);
+        assertEq(fleekeggs.balanceOf(user, 1), 4);
+        assertEq(fleekeggs.balanceOf(user, 2), 0);
+        assertEq(fleekeggs.balanceOf(user, 4), 0);
+    }
+}
